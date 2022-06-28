4a7d54dce5977637a060728d192bab247c9ba402
Danil <ddynikov@gmail.com>
2022-06-28 06:38:26 +0300

    fixed rendering by react

--- self-driving-car/src/App.js
+++ self-driving-car/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
+    <React.Fragment>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src='/images/map.png' className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        >Learn React
         </a>
       </header>
     </div>
+    </React.Fragment>
   );
 }
 
